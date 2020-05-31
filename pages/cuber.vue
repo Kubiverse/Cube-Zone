1c8910148d25795ace8f7cc37806f83c605c36a2
James <james@thecubicle.us>
2020-05-31 11:50:18 -0400

    Disable Cuber Profile Pbs tab

--- pages/cuber.vue
+++ pages/cuber.vue
@@ -65,14 +65,6 @@
           </v-tabs>
 
           <v-tabs-items v-model="tab">
-            <v-tab-item key="pbs">
-              <ViewCuberPbsInterface
-                :status="currentTab == 'pbs'"
-                :generation="generation.solves"
-                :cuber="cuber"
-              ></ViewCuberPbsInterface>
-            </v-tab-item>
-
             <v-tab-item key="solves">
               <v-row>
                 <v-col cols="3">
@@ -163,7 +155,7 @@ import {
   FOLLOW_CUBER_MUTATION,
   UNFOLLOW_CUBER_MUTATION,
 } from '~/gql/mutation/cuber.js'
-import ViewCuberPbsInterface from '~/components/interface/viewCuberPbsInterface.vue'
+//import ViewCuberPbsInterface from '~/components/interface/viewCuberPbsInterface.vue'
 import ViewCuberSolvesInterface from '~/components/interface/viewCuberSolvesInterface.vue'
 import ViewCuberRoomsInterface from '~/components/interface/viewCuberRoomsInterface.vue'
 import ViewCuberFollowersInterface from '~/components/interface/viewCuberFollowersInterface.vue'
@@ -172,7 +164,7 @@ import EventLabel from '~/components/shared/eventLabel.vue'
 
 export default {
   components: {
-    ViewCuberPbsInterface,
+    //ViewCuberPbsInterface,
     ViewCuberSolvesInterface,
     ViewCuberRoomsInterface,
     ViewCuberFollowersInterface,
@@ -207,7 +199,7 @@ export default {
 
       tab: null,
       tabItems: [
-        { tab: 'pbs', title: 'Personal Records' },
+        //{ tab: 'pbs', title: 'Personal Records' },
         { tab: 'solves', title: 'Solves' },
         { tab: 'rooms', title: 'Recent Rooms' },
         { tab: 'follows', title: 'Follows' },
