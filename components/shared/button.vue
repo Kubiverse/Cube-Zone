0d0842bcdfac60aacebf4fa84cc7662ea5076ddf
Zoe de Moffarts <zoe.demoffarts@gmail.com>
2020-06-21 21:37:11 +0200

    Create button component #19
    The text color on the buttons doesn't adapt to their background color and Vuetify makes it quite hard to customize that. So we need a custom component to help us do that.

---
+++ components/shared/button.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-btn
+    v-bind="$attrs"
+    :color="icon ? undefined : inversed ? textColor : parsedColor"
+    :style="
+      !icon &&
+      parsedVariant === 'contained' && {
+        color: inversed ? parsedColor : textColor,
+      }
+    "
+    :outlined="parsedVariant === 'outlined'"
+    :text="parsedVariant === 'text'"
+    :icon="icon"
+    @click="$emit('click', $event)"
+  >
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'primary',
+    },
+    variant: {
+      type: String,
+      default: 'contained',
+    },
+    inversed: {
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      colorToTextColorMap: {
+        dark: {
+          primary: '#121212',
+          secondary: '#121212',
+          accent: '#fff',
+          success: '#082218',
+          warning: '#2A150D',
+          error: '#0e0000',
+          info: '#050709',
+        },
+        light: {
+          primary: '#fff',
+          secondary: '#fff',
+          accent: '#121212',
+          success: '#082218',
+          warning: '#2A150D',
+          error: '#0e0000',
+          info: '#050709',
+        },
+      },
+    }
+  },
+  computed: {
+    parsedVariant: function () {
+      return this.isAcceptedVariant(this.variant) ? this.variant : 'contained'
+    },
+    parsedColor: function () {
+      const color = this.isAcceptedColor(this.color) ? this.color : 'primary'
+      return this.$vuetify.theme.currentTheme[color]
+    },
+    textColor: function () {
+      const theme = this.$vuetify.theme.dark ? 'dark' : 'light'
+      const color = this.isAcceptedColor(this.color) ? this.color : 'primary'
+      return this.colorToTextColorMap[theme][color]
+    },
+  },
+
+  methods: {
+    isAcceptedColor: (color) => {
+      return (
+        color === 'primary' ||
+        color === 'secondary' ||
+        color === 'accent' ||
+        color === 'info' ||
+        color === 'success' ||
+        color === 'warning' ||
+        color === 'error'
+      )
+    },
+    isAcceptedVariant: (variant) => {
+      return (
+        variant === 'contained' || variant === 'outlined' || variant === 'text'
+      )
+    },
+  },
+}
+</script>
